782580a9bceb72232e569eabb16da4ac222a37f5
Luca Danelutti <lucadanelutti@outlook.com>
2021-10-31 11:46:00 +0100

    [L04] Add maximum likelihood computations

--- L04/main.py
+++ L04/main.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import matlab.engine
 
 def read_dataset(file_name, webserver_id):
     file = open(file_name)
@@ -20,13 +20,15 @@ def read_dataset(file_name, webserver_id):
                         "Webserver 4": float(entry["Webserver 4"])})
     return [entry["Webserver " + str(webserver_id)] for entry in dataset]
 
-def compute(file_name, webserver_id):
-    print("Computing indices for %s" % file_name)
+def compute_moments(file_name, webserver_id):
+    print("Computing indices for %s webserver %s" % (file_name, str(webserver_id)))
 
-   # Read dataset and cast values 
+    # Read dataset and cast values 
     dataset = read_dataset(file_name, webserver_id)
     
     # Fit data against uniform, exponential, hyperexponential, hypoexponential using moments
+    print("Using moments:")
+    
     # Uniform: a and b
     def moment(n):
         cumul = 0
@@ -41,8 +43,8 @@ def compute(file_name, webserver_id):
     print("Uniform | b: %f" % b)
 
     # Exponential: lambda
-    lambda_ = 1 / first_moment
-    print("Exponential | lambda: %f" % lambda_)
+    exponential_lambda = 1 / first_moment
+    print("Exponential | lambda: %f" % exponential_lambda)
 
     # Hyperexponential: p, lambda1, lambda2
     p = 0.4
@@ -61,7 +63,7 @@ def compute(file_name, webserver_id):
     # Compute CDF of fitted distributions
     x = np.linspace(0, 50, 5000)
     uniform = (x - a) / (b - a)
-    exponential = 1 - np.power(np.e, - (lambda_ * x))
+    exponential = 1 - np.power(np.e, - (exponential_lambda * x))
     hyperexponential = 1 - \
         0.4 * np.power(np.e, - (hyper_lambda1 * x)) - \
         0.6 * np.power(np.e, - (hyper_lambda2 * x))
@@ -86,12 +88,86 @@ def compute(file_name, webserver_id):
     plt.ylim(bottom=0, top=1)
     plt.legend()
     plt.show()
+    # input("Press enter to continue")
+
+def compute_maximum_likelihood(file_name, webserver_id):
+    print("Computing indices for %s webserver %s" % (file_name, str(webserver_id)))
+
+    # Read dataset and cast values
+    dataset = read_dataset(file_name, webserver_id)
+    
+    # Fit data against uniform, exponential, hyperexponential, hypoexponential using maximum likelihood
+    print("Using maximum likelihood:")
+
+    # Exponential: lambda
+    cumul = 0
+    for time in dataset:
+        cumul += time
+    exponential_lambda = len(dataset) / cumul
+    print("Exponential: %f" % exponential_lambda)
+
+    # Setting up matlab engine
+    eng = matlab.engine.start_matlab()
+    matlab_dataset = matlab.double(dataset)
+    eng.workspace['dataset'] = matlab_dataset
+
+    # Hyperexponential: p, lambda1, lambda2
+    estimations = eng.eval(
+        "mle(dataset, 'pdf', @(x, l1, l2, p) \
+             p * l1 * exp(-l1 * x) + (1-p) * l2 * exp(-l2 * x), 'Start', [0.5, 0.5, 0.5], 'LowerBound', [0, 0, 0], 'UpperBound', [Inf, Inf, 1])")
+    hyper_lambda1 = estimations[0][0]
+    hyper_lambda2 = estimations[0][1]
+    p = estimations[0][2]
+    print("Hyperexponential | p: %f" % p)
+    print("Hyperexponential | lambda1: %f" % hyper_lambda1)
+    print("Hyperexponential | lambda2: %f" % hyper_lambda2)
+
+    # Hypoexponential: lambda1, lambda2
+    estimations = eng.eval(
+        "mle(dataset, 'pdf', @(x, l1, l2) \
+             (l1 * l2) / (l1 - l2) * (exp(-l2 * x) - exp(-l1 * x)), 'Start', [0.45, 0.55], 'LowerBound', [0, 0], 'UpperBound', [Inf, Inf])")
+    hypo_lambda1 = estimations[0][0]
+    hypo_lambda2 = estimations[0][1]
+    print("Hypoexponential | lambda1: %f" % hypo_lambda1)
+    print("Hypoexponential | lambda2: %f" % hypo_lambda2)
+
+    # Quitting matlab engine
+    eng.quit()
+
+    # Compute CDF of fitted distributions
+    x = np.linspace(0, 50, 5000)
+    exponential = 1 - np.power(np.e, - (exponential_lambda * x))
+    hyperexponential = 1 - \
+        0.4 * np.power(np.e, - (hyper_lambda1 * x)) - \
+        0.6 * np.power(np.e, - (hyper_lambda2 * x))
+    hypoexponential = 1 - \
+        (hypo_lambda2 * np.power(np.e, - (hypo_lambda1 * x))) / (hypo_lambda2 - hypo_lambda1) + \
+        (hypo_lambda1 * np.power(np.e, - (hypo_lambda2 * x))) / \
+        (hypo_lambda2 - hypo_lambda1)
+
+    # Compute CDF of dataset
+    ordered_dataset = sorted(dataset)
+    cdf = []
+    i = 1/len(ordered_dataset)
+    for time in ordered_dataset:
+        cdf.append((time, i))
+        i += 1/len(ordered_dataset)
+
+    # Plot CDFs
+    plt.plot([item[0] for item in cdf], [item[1]
+                                         for item in cdf], label="Dataset")
+    plt.plot(x, exponential, label="Exp")
+    plt.plot(x, hyperexponential, label="Hyperexp")
+    plt.plot(x, hypoexponential, label="Hypoexp")
+    plt.ylim(bottom=0, top=1)
+    plt.legend()
+    plt.show()
     input("Press enter to continue")
 
-    # Fit data against uniform, exponential, hyperexponential, hypoexponential using maximum likelihood
-
 def run():
-    compute("L04/Traces.csv", 1)
+    compute_maximum_likelihood("L04/Traces.csv", 1)
+    print("---")
+    compute_moments("L04/Traces.csv", 1)
 
 if __name__ == '__main__':
     run()
