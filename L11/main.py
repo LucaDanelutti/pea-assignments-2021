d794eed52bdc10fd31a481df177f29d0c76f17f1
Luca Danelutti <lucadanelutti@outlook.com>
2021-11-05 09:13:43 +0100

    [L11] Separate transient and stationary solutions

--- L11/main.py
+++ L11/main.py
@@ -27,35 +27,48 @@ def compute():
     s0[0] = 0.4
     s0[1] = 0.6
 
+    # Define differential equation
+    def equation(y, x):
+        dydt = np.dot(y, infinitesimal_generator)
+        return dydt
+
+    # Define u
+    u = np.zeros((7))
+    u[0] = 1
+
     # Define Q'
     infinitesimal_generator_prime = np.copy(infinitesimal_generator)
     for i in range(0, 7):
         infinitesimal_generator_prime[i][0] = 1
 
     # Compute stationary solution
-    y_stationary = np.matmul(s0, np.linalg.inv(infinitesimal_generator_prime))
+    y_stationary = np.matmul(u, np.linalg.inv(infinitesimal_generator_prime))
 
-    # Define differential equation
-    def equation(y, x):
-        dydt = np.dot(y, infinitesimal_generator)
-        return dydt
-
+    # Compute transient solution
     # Define x
     x = np.linspace(0, 1000000, 1000000)
     # Solve differential equation
     y = odeint(equation, s0, x)
 
-    # Print probabilities
-    print("Stationary probability of being computing: %f" %
+    # Print probabilities using transient solution at t=100000
+    print("t=1000000 probability of being computing: %f" %
           (y[999999][0] + y[999999][1]))
-    print("Stationary probability of being WiFi: %f" %
+    print("t=1000000 probability of being WiFi: %f" %
           (y[999999][2] + y[999999][3]))
-    print("Stationary probability of being 4G: %f" %
+    print("t=1000000 probability of being 4G: %f" %
           (y[999999][4] + y[999999][5] + y[999999][6]))
 
-    print("Throughput: %f" % (y[999999][6]*1*(infinitesimal_generator[6][0] +
-        infinitesimal_generator[6][1]) + y[999999][2]*1*(infinitesimal_generator[2][0] +
-        infinitesimal_generator[2][1]) + y[999999][3]*1*(infinitesimal_generator[3][0] +
+    # Print probabilities using stationary solution
+    print("Stationary probability of being computing: %f" %
+          (y_stationary[0] + y_stationary[1]))
+    print("Stationary probability of being WiFi: %f" %
+          (y_stationary[2] + y_stationary[3]))
+    print("Stationary probability of being 4G: %f" %
+          (y_stationary[4] + y_stationary[5] + y_stationary[6]))
+
+    print("Throughput: %f" % (y_stationary[6]*1*(infinitesimal_generator[6][0] +
+        infinitesimal_generator[6][1]) + y_stationary[2]*1*(infinitesimal_generator[2][0] +
+        infinitesimal_generator[2][1]) + y_stationary[3]*1*(infinitesimal_generator[3][0] +
         infinitesimal_generator[3][1])))
 
 def run():
